573b030cbc32493a0b3f63d6e29b95159d9e16a5
David Molineus <david.molineus@netzmacht.de>
2017-07-03 09:57:38 +0200

    Add contao manager plugin.

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package    contao-font-awesome-inserttag
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\FontAwesomeInsertTag\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Netzmacht\Contao\FontAwesomeInsertTag\NetzmachtFontAwesomeInsertTagBundle;
+
+/**
+ * Class Plugin.
+ *
+ * @package Netzmacht\Contao\FontAwesomeInsertTag\ContaoManager
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(NetzmachtFontAwesomeInsertTagBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
